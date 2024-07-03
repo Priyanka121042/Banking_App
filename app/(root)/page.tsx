88561c4031d07fc43cde2e1916e5cb1dbc2a3dcc
Priyanka <priyanka.121042@gmail.com>
2024-07-04 00:47:10 +0530

    card commits

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
+import RightSidebar from '@/components/RightSidebar'
 import HeaderBox from '@/components/ui/HeaderBox'
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-    const loggedIn= {firstName: 'Priyanka'}
+    const loggedIn= {firstName: 'Priyanka', lastName: 'Ranjan', email: 'contact@user.pro'}
   return (
     <section className='home'> 
     <div className='home-content'>
@@ -19,11 +20,17 @@ const Home = () => {
           accounts={[]}
           totalBanks={1}
           totalCurrentBalance={1250.35}
-          >
+          />
 
-          </TotalBalanceBox>
       </header>
+
+      RECENT TRANSACTIONS
     </div>
+
+    <RightSidebar 
+    user={loggedIn}
+    transactions={[]}
+    banks={[{ currentBalance: 123.50}, {currentBalance: 500.50}]}/>
     </section>
   )
 }
